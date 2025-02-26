c7f70e02406a3b61d64c70dd2a036eaf5952ecdf
its-topdev <neil.zhao.adams@gmail.com>
2025-02-26 08:49:54 -0600

    Fixed routing rule by directing it to login page for guests

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import { useState, useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import UserContext from './contexts/UserContext';
-import WindowSizeContext from './contexts/WindowSizeContext';
+import { useState, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "./contexts/UserContext";
+import WindowSizeContext from "./contexts/WindowSizeContext";
 import { userRoles } from "./constants/userRoles";
-import Auth from './components/Auth';
-import Home from './components/_pages/Home';
-import Login from './components/_pages/Login';
-import ForgotPassword from './components/_pages/Login/ForgotPassword';
-import ResetPassword from './components/_pages/Login/ResetPassword';
-import Registration from './components/_pages/Registration';
-import Verification from './components/_pages/Registration/Verification';
-import BookConfirmation from './components/_pages/BookConfirmation';
-import Management from './components/Management';
-import NotFound from './components/_pages/NotFound';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import Auth from "./components/Auth";
+import Home from "./components/_pages/Home";
+import Login from "./components/_pages/Login";
+import ForgotPassword from "./components/_pages/Login/ForgotPassword";
+import ResetPassword from "./components/_pages/Login/ResetPassword";
+import Registration from "./components/_pages/Registration";
+import Verification from "./components/_pages/Registration/Verification";
+import BookConfirmation from "./components/_pages/BookConfirmation";
+import Management from "./components/Management";
+import NotFound from "./components/_pages/NotFound";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
-  const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+  const user = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null;
   const [currentUser, setCurrentUser] = useState(user);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
@@ -30,37 +28,48 @@ function App() {
     const handleResize = () => {
       setWindowHeight(window.innerHeight);
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-    }
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <>
-    <WindowSizeContext.Provider value={{windowHeight}}>
-      <UserContext.Provider  value={{currentUser, setCurrentUser}}>
+      <WindowSizeContext.Provider value={{ windowHeight }}>
+        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
           <BrowserRouter>
-              <Routes>
-                <Route path="*" element={<NotFound />} />
-                {/* <Route element={<Auth allowedRoles={[userRoles.ADMIN, userRoles.USER]} />}> */}
-                <Route>
-                  <Route path="/" element={<Home />} />
-                </Route>
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="/registration" element={<Registration />} />  
-                <Route path="/verification" element={<Verification />} />
-                {/* <Route element={<Auth allowedRoles={[userRoles.ADMIN, userRoles.USER]} />}> */}
-                <Route>
-                  <Route path="/book-confirmation" element={<BookConfirmation />} />
-                </Route>
-                <Route element={<Auth allowedRoles={[userRoles.ADMIN]} />}>
-                  <Route path="/management/*" element={<Management />} />
-                </Route>
-              </Routes>
+            <Routes>
+              <Route path="*" element={<NotFound />} />
+              <Route
+                element={
+                  <Auth allowedRoles={[userRoles.ADMIN, userRoles.USER]} />
+                }
+              >
+                {/* <Route> */}
+                <Route path="/" element={<Home />} />
+              </Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/registration" element={<Registration />} />
+              <Route path="/verification" element={<Verification />} />
+              <Route
+                element={
+                  <Auth allowedRoles={[userRoles.ADMIN, userRoles.USER]} />
+                }
+              >
+                {/* <Route> */}
+                <Route
+                  path="/book-confirmation"
+                  element={<BookConfirmation />}
+                />
+              </Route>
+              <Route element={<Auth allowedRoles={[userRoles.ADMIN]} />}>
+                <Route path="/management/*" element={<Management />} />
+              </Route>
+            </Routes>
           </BrowserRouter>
         </UserContext.Provider>
       </WindowSizeContext.Provider>
@@ -72,7 +81,7 @@ function App() {
         pauseOnHover
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Auth/index.jsx
+++ src/components/Auth/index.jsx
@@ -1,18 +1,29 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { useLocation, Navigate, Outlet } from "react-router-dom";
+import PropTypes from "prop-types";
 import UserContext from "../../contexts/UserContext";
 
 const Auth = ({ allowedRoles }) => {
   const { currentUser } = useContext(UserContext);
   const location = useLocation();
 
-  return currentUser && currentUser.role ? ( allowedRoles.find((role) => currentUser.role.includes(role)) ? (
-    <Outlet />
-  ) : (currentUser.name ? (
-    <Navigate to="/" state={{ from: location }} replace />
+  console.log(currentUser);
+
+  return currentUser && currentUser.role ? (
+    allowedRoles.find((role) => currentUser.role.includes(role)) ? (
+      <Outlet />
+    ) : currentUser.name ? (
+      <Navigate to="/" state={{ from: location }} replace />
+    ) : (
+      <Navigate to="/login" state={{ from: location }} replace />
+    )
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
-  ))) : <Navigate to="/login" state={{ from: location }} replace />;
+  );
+};
+
+Auth.propTypes = {
+  allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Auth;
