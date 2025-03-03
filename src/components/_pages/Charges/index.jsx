5a1f41047d6e180588d4c9eeb0c982acfcb8eca6
its-topdev <neil.zhao.adams@gmail.com>
2025-03-03 19:43:45 +0300

    Allowed user authentication in charge api

--- src/components/_pages/Charges/index.jsx
+++ src/components/_pages/Charges/index.jsx
@@ -9,73 +9,103 @@ import useFetch from "../../../hooks/useFetch";
 import "./style.scss";
 
 export default function ChargesPage() {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageSize, setPageSize] = useState(10);
-    const [isShowing, toggle, setIsShowing] = useModal('');
-    const {data: chargesData, loading: chargesLoading, error, fetchData: fetchCharg} = useFetch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [isShowing, toggle, setIsShowing] = useModal("");
+  const {
+    data: chargesData,
+    loading: chargesLoading,
+    fetchData: fetchCharg,
+  } = useFetch();
 
-    const fetchCharges = () => {
-        try {
-            fetchCharg(`${API_URL}charge/list?page=${currentPage}&pageSize=${pageSize}`, 'get');        
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const fetchCharges = () => {
+    try {
+      fetchCharg(
+        `${API_URL}charge/list?page=${currentPage}&pageSize=${pageSize}`,
+        "get",
+        undefined,
+        true
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        fetchCharges();
-    }, [currentPage, pageSize]);
+  useEffect(() => {
+    fetchCharges();
+  }, [currentPage, pageSize]);
 
-    const rows = [];
-    const charges = chargesData && chargesData.charges;
-    const totalCount = chargesData && chargesData.totalCount;
-    charges && charges.forEach((charge) => {
-        rows.push(
+  const rows = [];
+  const charges = chargesData && chargesData.charges;
+  const totalCount = chargesData && chargesData.totalCount;
+  charges &&
+    charges.forEach((charge) => {
+      rows.push(
         <ChargeRow
-            charge={charge}
-            key={charge.id}
-            onFetchCharges={fetchCharges}
-            onSetIsShowingModal={setIsShowing} 
-            />
-        );
+          charge={charge}
+          key={charge.id}
+          onFetchCharges={fetchCharges}
+          onSetIsShowingModal={setIsShowing}
+        />
+      );
     });
 
-    const onCloseButtonClick = () => {
-        toggle();
-    }
+  const onCloseButtonClick = () => {
+    toggle();
+  };
 
-    return (
-        <>
-            <div className="management-body-top">
-                <h1 className="management-body-title">Charges</h1>
-                <button type="button" className="button-blue-1 charges-new-button" onClick={() => setIsShowing(true)}>Create New</button>
-            </div>
-            <div className="management-body-table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>name</th>
-                            <th>Price</th>
-                            <th>Type</th>
-                            <th className="action"></th>
-                            <th className="action"></th>
-                        </tr>
-                    </thead>
-                    <tbody>{chargesLoading ? <tr><td><Loader /></td></tr> : rows }</tbody>
-                </table>
-            </div>
-            <div className="management-body-pagination">
-                <Pagination
-                    currentPage={currentPage}
-                    onSetCurrentPage={setCurrentPage}
-                    pageSize={pageSize}
-                    onSetPageSize={setPageSize}
-                    totalCount={totalCount}
-                />
-            </div>
-            { isShowing && 
-                <ChargeAddModal show={isShowing} onFetchCharges={fetchCharges} onCloseButtonClick={onCloseButtonClick} />
-            }
-        </>
-    );
+  return (
+    <>
+      <div className="management-body-top">
+        <h1 className="management-body-title">Charges</h1>
+        <button
+          type="button"
+          className="button-blue-1 charges-new-button"
+          onClick={() => setIsShowing(true)}
+        >
+          Create New
+        </button>
+      </div>
+      <div className="management-body-table">
+        <table>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>Price</th>
+              <th>Type</th>
+              <th className="action"></th>
+              <th className="action"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {chargesLoading ? (
+              <tr>
+                <td>
+                  <Loader />
+                </td>
+              </tr>
+            ) : (
+              rows
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="management-body-pagination">
+        <Pagination
+          currentPage={currentPage}
+          onSetCurrentPage={setCurrentPage}
+          pageSize={pageSize}
+          onSetPageSize={setPageSize}
+          totalCount={totalCount}
+        />
+      </div>
+      {isShowing && (
+        <ChargeAddModal
+          show={isShowing}
+          onFetchCharges={fetchCharges}
+          onCloseButtonClick={onCloseButtonClick}
+        />
+      )}
+    </>
+  );
 }
